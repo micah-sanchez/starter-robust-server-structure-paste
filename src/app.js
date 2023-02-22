aa04edf2e5cba2f323ad1a41f0da35fe409a66a2
micah-sanchez <mjsanch4@gmail.com>
2023-02-21 20:34:48 -0700

    added pasteId route

--- src/app.js
+++ src/app.js
@@ -4,6 +4,17 @@ const app = express();
 // TODO: Follow instructions in the checkpoint to implement ths API.
 const pastes = require("./data/pastes-data");
 
+app.use("/pastes/:pasteId", (req, res, next) => {
+  const { pasteId } = req.params;
+  const foundPaste = pastes.find((paste) => paste.id === Number(pasteId));
+
+  if (foundPaste) {
+    res.json({ data: foundPaste });
+  } else {
+    next(`Paste id not found: ${pasteId}`);
+  }
+});
+
 app.use("/pastes", (req, res) => {
   res.json({ data: pastes });
 });
